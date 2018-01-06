17bc1575be7ca805ac82bc05e797f4e31bb35a89
DwiRomadon <dwi.13421054@student.ubl.ac.id>
2018-01-06 14:10:41 +0700

    update v2

--- Api_Siater/v2/include/Config.php
+++ Api_Siater/v2/include/Config.php
@@ -5,18 +5,5 @@
 /**
  * Database config variables
  */
-/*define("DB_HOST", "103.230.48.154:60000");
-define("DB_USER", "adminnyaubl");
-define("DB_PASSWORD", "muhamadfarhanalzakwan");
-define("DB_DATABASE", "siuacademic");*/
 
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
-define("DB_DATABASE", "siuacademic");
-
-/*define("DB_HOST", "202.162.205.229:3165");
-define("DB_USER", "c1siater5user");
-define("DB_PASSWORD", "ejf@W6R7");
-define("DB_DATABASE", "c1siater5maindb");*/
 ?>
